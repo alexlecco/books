6a6f1715835fe7841704eb96f7457ef3293e152f
alexlecco <alexlecco@gmail.com>
2017-10-28 11:13:14 -0300

    eliminamos campo de la db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028132741) do
+ActiveRecord::Schema.define(version: 20171028140432) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.text "description"
-    t.string "edition"
     t.date "publication"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
