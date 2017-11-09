bdef9fe2f13ff6e7bc222fe9baaaac11accdcc03
alexlecco <alexlecco24@gmail.com>
2017-11-09 00:14:02 -0300

    se agrego la relacion entre category y book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -45,6 +45,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :author, :description, :publication_date, :price)
+    params.require(:book).permit(:title, :author, :description, :publication_date, :price, :category_id)
   end
 end
